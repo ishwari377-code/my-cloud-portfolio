f82047d4efbd7690982cbc8dc6f25faf32a4eb4f
eshwari <ishwarideokule377@gmail.com>
2026-07-28 02:38:06 +0530

    My first Python script

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("-----------------------------------------")
+print("🚀 My cloud journey has officially started!")
+print("💪 Environment check: Success!")
+print("-----------------------------------------")
